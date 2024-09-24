2ef005b28491cfdc0001eb1136944a3c01c9eaa2
w0846568 <w0846568@myscc.ca>
2024-09-24 14:24:20 -0400

    Question 8.5, Trying to get the application to function.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Gem } from './gem';
-import {NgFor} from '@angular/common';
+import { NgFor } from '@angular/common';
 import { NgIf } from '@angular/common';
-
-
+import { GemListComponent } from "./gem-list/gem-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, NgIf],
+  imports: [RouterOutlet, NgFor, NgIf, GemListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
