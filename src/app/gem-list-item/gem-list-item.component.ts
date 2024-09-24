effe9c61e0c2f993dd52b488337f3a7f66f930ba
w0846568 <w0846568@myscc.ca>
2024-09-24 17:17:40 -0400

    Stuck on question 9

--- src/app/gem-list-item/gem-list-item.component.ts
+++ src/app/gem-list-item/gem-list-item.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Gem } from '../gem';
+import { NgFor } from '@angular/common';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-gem-list-item',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './gem-list-item.component.html',
   styleUrl: './gem-list-item.component.css'
 })
 export class GemListItemComponent {
-
+  @Input()gem?:Gem;
 }
--- src/app/gem-list/gem-list.component.ts
+++ src/app/gem-list/gem-list.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Gem } from '../gem';
+import { NgFor } from '@angular/common';
+import { NgIf } from '@angular/common';
+import { GemListItemComponent } from '../gem-list-item/gem-list-item.component'; 
 
 @Component({
   selector: 'app-gem-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf, GemListItemComponent],
   templateUrl: './gem-list.component.html',
   styleUrl: './gem-list.component.css'
 })
 export class GemListComponent {
+  opal : Gem = {id: 7, material: "Opal", price: 75.99, productionCompany: "Rock Bottom", rarity : "rare"};
+  garnet : Gem = {id: 8, material: "Garnet", price: 65.00, productionCompany: "Rock Bottom", rarity : "uncommon"};
+  beryl :  Gem = {id: 7, material: "Opal", price: 75.99, productionCompany: "Rock Bottom", rarity : "rare"};
+  aquamarine : Gem = {id: 7, material: "Opal", price: 75.99, productionCompany: "Rock Bottom", rarity : "rare"};
+  gems2 = [this.opal, this.garnet, this.beryl, this.aquamarine];
+
 
 }
